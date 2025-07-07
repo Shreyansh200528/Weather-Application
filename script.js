af7d24cb18efd4538a5295fc57794b0081fe4c4e
Shreyansh200528 <145379551+Shreyansh200528@users.noreply.github.com>
2025-07-07 21:54:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "54cea9dfe05725f8ef2499e0fe72fb42";
+    const api_key = "your google api key";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
